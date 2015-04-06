fab22693706f578d352844f8997e992e26ea7783
emanuel <emanuelmetal@gmail.com>
2015-04-06 20:22:00 -0300

    Added logo, nginx conf file, uwsgi sh.

---
+++ CruzadaSite/uwsgi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASEDIR=$( cd $(dirname $0) ; pwd)
+echo $BASEDIR
+if [ -z "$VIRTUAL_ENV" -a -d $BASEDIR/../virtualenv ] ; then
+        . $BASEDIR/../virtualenv/bin/activate
+fi
+
+cd $BASEDIR
+
+uwsgi --module=FileManagerUISite.wsgi:application \
+--env DJANGO_SETTINGS_MODULE=FileManagerUISite.settings \
+--master --processes=1 --enable-threads \
+--home=${VIRTUAL_ENV} --socket=127.0.0.1:8001 \
+--python-path=${BASEDIR} \
+--static-map /static=${BASEDIR}/FileManagerUI/static
