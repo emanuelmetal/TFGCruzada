4dfca13bf2657afb8b1379dae99a4325618640f8
emanuelmetal <emanuelmetal@gmail.com>
2015-04-07 12:29:28 -0300

    Fixed uwsgi script

--- CruzadaSite/uwsgi.sh
+++ CruzadaSite/uwsgi.sh
@@ -8,9 +8,9 @@ fi
 
 cd $BASEDIR
 
-uwsgi --module=FileManagerUISite.wsgi:application \
---env DJANGO_SETTINGS_MODULE=FileManagerUISite.settings \
+uwsgi --module=CruzadaSite.wsgi:application \
+--env DJANGO_SETTINGS_MODULE=CruzadaSite.settings \
 --master --processes=1 --enable-threads \
 --home=${VIRTUAL_ENV} --socket=127.0.0.1:8001 \
 --python-path=${BASEDIR} \
---static-map /static=${BASEDIR}/FileManagerUI/static
+--static-map /static=${BASEDIR}/Cruzada/static
